d99bb11b921bcb61d98d0726c3a7d83ec36530ce
Darshan <darshan@bigscal.com>
2026-04-13 12:18:30 +0530

    defined  routes for each method we created under controller of employee resource.

---
+++ backend/src/routes/employees.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+function createEmployeeRouter(controller) {
+  const router = express.Router();
+
+  router.post('/', asyncHandler((req, res, next) => controller.create(req, res, next)));
+  router.get('/', asyncHandler((req, res, next) => controller.findAll(req, res, next)));
+  router.get('/:id', asyncHandler((req, res, next) => controller.findById(req, res, next)));
+  router.patch('/:id', asyncHandler((req, res, next) => controller.update(req, res, next)));
+  router.delete('/:id', asyncHandler((req, res, next) => controller.remove(req, res, next)));
+
+  return router;
+}
+
+module.exports = { createEmployeeRouter };
